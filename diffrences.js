66cfdc36a2220bce6f5f7837899163f0715a57eb
vimalAdithan <vimaladithanrayz1213s.v.t@gmail.com>
2023-04-25 10:11:46 +0530

    first commit

---
+++ diffrences.js
@@ -0,0 +1,13 @@
+/*
+WINDOW:
+Window object holds the document and screen where else window is the parent of document and screen object.
+Window is the tab of the page in the device
+
+DOCUMENT:
+Which contains the document or the information which is viewed on the window,it holds certain properties and methods to navigate the page.
+Mostly it is an HTML document.
+
+SCREEN:
+Screen is the object which takes care of the dimensions of the window,resolution,etc.
+Screen object makes the window responsive.
+*/
---
+++ json_format.js
@@ -0,0 +1,20 @@
+// INPUT:
+var obj = {
+  "name": "arun",
+  "age": "22",
+  "location": "China",
+  "education": "Graduate in Computer Application",
+  "skills": ["Html", "css", "javascript"],
+  "Experience": "3years",
+};
+console.log(obj);
+
+/*
+OUTPUT:
+{ name: 'arun',
+  age: '22',
+  location: 'China',
+  education: 'Graduate in Computer Application',
+  skills: [ 'Html', 'css', 'javascript' ],
+  Experience: '3years' }
+*/
